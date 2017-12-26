3b217fb1f02b880201731ce3ac2241143908e1cd
hoangkhai6893 <hoangkhai6893@gmail.com>
2017-12-26 10:03:12 +0700

    ADD file FailSafe

---
+++ SOURCE/FailSafe.h
@@ -0,0 +1,18 @@
+// FailSafe.h
+
+#ifndef _FAILSAFE_h
+#define _FAILSAFE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "RAM.h"
+
+
+
+
+#endif
+
